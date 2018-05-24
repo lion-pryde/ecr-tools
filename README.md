adf0bd83e0b3b97360b76575e59f604989106a3b
squizr <jacob@squires.xyz>
2018-05-25 07:34:44 +1000

    WIP

--- README.md
+++ README.md
@@ -2,18 +2,26 @@
 
 [![Go Report Card](https://goreportcard.com/badge/github.com/lyon-pryde/ecr-tools)](https://goreportcard.com/report/github.com/lyon-pryde/ecr-tools)
 
+## TL;DR
+
 Small container for generating docker token for ECR.
 
 ```sh
 docker run pryde/ecr-tools get-token
 ```
+
 Priority of config.
 
-1) AWS Config file
-2) Environment Variables
-3) binary flags
+- AWS Config files.
 
-> Required claims
+- - [~/.aws/config](~/.aws/config)
+
+- - [~/.aws/credentials](~/.aws/credentials)
+
+- Environment Variables
+- Binary flags
+
+## Required claims
 
 ```json
 [
@@ -30,4 +38,26 @@ Priority of config.
     "ecr:CompleteLayerUpload",
     "ecr:PutImage"
 ]
+```
+
+## Examples
+
+```sh
+docker run pryde/ecr-tools get-token -i
+```
+
+```bash
+bash-3.2$ go run main.go --help
+  -aws-access-id string
+        AWS Access ID overrides env variable AWS_ACCESS_KEY_ID
+  -aws-default-region string
+        AWS Default Region overrides env variable AWS_DEFAULT_REGION
+  -aws-secret-access-key string
+        AWS Access ID overrides env variable AWS_ACCESS_KEY_ID
+  -i string
+        AWS Access ID overrides env variable AWS_ACCESS_KEY_ID (shorthand)
+  -k string
+        AWS Access ID overrides env variable AWS_ACCESS_KEY_ID (shorthand)
+  -r string
+        AWS Default Region overrides env variable AWS_DEFAULT_REGION (shorthand)
 ```
--- cmd/ecr-tools/main.go
+++ cmd/ecr-tools/main.go
@@ -1,72 +1,112 @@
 package main
 
 import (
-	"github.com/aws/aws-sdk-go-v2/aws/awserr"
-	"github.com/aws/aws-sdk-go-v2/aws/external"
-	"github.com/aws/aws-sdk-go-v2/service/ecr"
-
 	"flag"
 	"fmt"
 	"os"
-)
 
-var awsAccessID, awsSecretAccessKey, awsDefaultRegion string
+	"github.com/aws/aws-sdk-go-v2/aws"
+	"github.com/aws/aws-sdk-go-v2/service/ecr"
+	"github.com/aws/aws-sdk-go-v2/aws/awserr"
+	"github.com/lyon-pryde/ecr-tools/pkg/collector"
+	"plugin"
+	"github.com/aws/aws-sdk-go-v2/aws/external"
+)
+var AWSAccessID      = os.Getenv("AWS_ACCESS_KEY_ID")
+var AWSDefaultRegion      = os.Getenv("AWS_DEFAULT_REGION")
+var AWSSecretAccessKey      = os.Getenv("AWS_SECRET_ACCESS_KEY")
+
+var AWSAccessIDUsage = "AWS Access ID overrides env variable AWS_ACCESS_KEY_ID"
+var AWSSecretAccessKeyUsage = "AWS Access ID overrides env variable AWS_SECRET_ACCESS_KEY"
+var AWSDefaultRegionUsage = "AWS Default Region overrides env variable AWS_DEFAULT_REGION"
+
+var configFlags = flag.NewFlagSet("get-token", flag.ExitOnError)
+
+var awsAccessID = configFlags.String("aws-access-id", AWSAccessID, AWSAccessIDUsage)
+var awsSecretAccessKey = configFlags.String("aws-secret-access-key", AWSSecretAccessKey, AWSSecretAccessKeyUsage)
+var awsDefaultRegion = configFlags.String("aws-default-region", AWSDefaultRegion, AWSDefaultRegionUsage)
 
 func init() {
-	var (
-		AWSAccessID      = os.Getenv("AWS_ACCESS_KEY_ID")
-		AWSAccessIDUsage = "AWS Access ID overrides env variable AWS_ACCESS_KEY_ID"
-	)
-	flag.StringVar(&awsAccessID, "aws-access-id", AWSAccessID, AWSAccessIDUsage)
-	flag.StringVar(&awsAccessID, "i", AWSAccessID, AWSAccessIDUsage+" (shorthand)")
-
-	var (
-		AWSSecretAccessKey      = os.Getenv("AWS_ACCESS_KEY_ID")
-		AWSSecretAccessKeyUsage = "AWS Access ID overrides env variable AWS_ACCESS_KEY_ID"
-	)
-	flag.StringVar(&awsSecretAccessKey, "aws-secret-access-key", AWSSecretAccessKey, AWSSecretAccessKeyUsage)
-	flag.StringVar(&awsSecretAccessKey, "k", AWSSecretAccessKey, AWSSecretAccessKeyUsage+" (shorthand)")
-
-	var (
-		AWSDefaultRegion      = os.Getenv("AWS_DEFAULT_REGION")
-		AWSDefaultRegionUsage = "AWS Default Region overrides env variable AWS_DEFAULT_REGION"
-	)
-	flag.StringVar(&awsDefaultRegion, "aws-default-region", AWSDefaultRegion, AWSDefaultRegionUsage)
-	flag.StringVar(&awsDefaultRegion, "r", AWSDefaultRegion, AWSDefaultRegionUsage+" (shorthand)")
+	configFlags.StringVar(awsAccessID, "id", "", "Description")
+	configFlags.StringVar(awsSecretAccessKey, "key", "", "Description")
+	configFlags.StringVar(awsDefaultRegion, "region", "", "Description")
 }
 
 func main() {
-	flag.Parse()
+	configFlags.Parse(os.Args[2:])
+
+	var AccessID string
+	AccessID = *awsAccessID
+	var SecretAccessKey string
+	SecretAccessKey = *awsSecretAccessKey
+	var DefaultRegion string
+	DefaultRegion = *awsDefaultRegion
+
+	credentialsProvider := Provider{
+		awsAccessID:        AccessID,
+		awsSecretAccessKey: SecretAccessKey,
+		awsDefaultRegion:   DefaultRegion,
+	}
+	fmt.Println(credentialsProvider)
 	switch os.Args[1] {
-	case "get-token":
-		getToken()
-	default:
-		fmt.Println("Command not found.")
+		case "get-token":
+			valid, err := credentialsProvider.AuthValid()
+
+			getToken(credentialsProvider)
+			if valid {
+				if err != nil {
+					fmt.Println("fuck")
+				}else{
+					fmt.Println("Invalid Auth")
+					flag.PrintDefaults()
+					os.Exit(67)
+				}
+			} else {
+				fmt.Println(err)
+				flag.PrintDefaults()
+				os.Exit(66)
+			}
+		default:
+			flag.PrintDefaults()
 	}
 }
 
-func getToken() {
-	// cfg:
-	// 	Config{}
+func getToken(p Provider) {
+	// Open plugin, and load it into the process.
+	//p, err := plugin.Open("./plugin/plugin.so")
+	//if err != nil {
+	//	exitErrorf("failed to open plugin, %s, %v", pluginFilename, err)
+	//}
+	// Create a new Credentials value which will source the provider's Retrieve
+	// and IsExpired functions from the plugin.
+	//creds, err := plugincreds.NewCredentials(p)
+	creds := aws.Credentials{
+		AccessKeyID:
+	}
+	// Load the config and set with the newly created credentials that
+	// will be sourced using the plugin's functionality.
 	cfg, err := external.LoadDefaultAWSConfig()
 	if err != nil {
-		panic("Unable to load SDK config, " + err.Error())
+		exitErrorf("failed to load config, %v", err)
 	}
-	svc := ecr.New(aws.Config{
-		Region: cfg.Region
-	})
+	cfg.Credentials = creds
+
+
+
+	fmt.Println("Config", cfg)
+	svc := ecr.New(cfg)
 	input := &ecr.GetAuthorizationTokenInput{}
 
 	result, err := svc.GetAuthorizationTokenRequest(input).Send()
 	if err != nil {
 		if aerr, ok := err.(awserr.Error); ok {
 			switch aerr.Code() {
-			case ecr.ErrCodeServerException:
-				fmt.Println(ecr.ErrCodeServerException, aerr.Error())
-			case ecr.ErrCodeInvalidParameterException:
-				fmt.Println(ecr.ErrCodeInvalidParameterException, aerr.Error())
-			default:
-				fmt.Println(aerr.Error())
+				case ecr.ErrCodeServerException:
+					fmt.Println(ecr.ErrCodeServerException, aerr.Error())
+				case ecr.ErrCodeInvalidParameterException:
+					fmt.Println(ecr.ErrCodeInvalidParameterException, aerr.Error())
+				default:
+					fmt.Println(aerr.Error())
 			}
 		} else {
 			// Print the error, cast err to awserr.Error to get the Code and
@@ -75,6 +115,8 @@ func getToken() {
 		}
 		return
 	}
-
-	fmt.Println(result.AuthorizationData)
+	fmt.Println(result)
+	for _, token := range result.AuthorizationData {
+		fmt.Println(token.AuthorizationToken)
+	}
 }
